2bca1a6b9c5a38588eba0ccb465e62882217f6e9
Wes Williams <wwwestech@gmail.com>
2013-07-25 22:04:53 -0600

    First attempt at OCR

---
+++ images/extractText.rb
@@ -0,0 +1,36 @@
+# NOTICE: Windows Dependent
+#####################################################################################################
+# Helpful Link:  http://rubyonwindows.blogspot.com/2009/08/ocr-converting-images-to-text-with-modi.html
+# Install MODI: http://support.microsoft.com/kb/982760
+#####################################################################################################
+# TODO: Reimplement to be platform agnostic
+
+require 'win32ole'
+require 'json'
+
+data = {}
+ARGV.each do |p|
+  if File.directory?(p)
+    files = Dir.entries(p).select { |s| !File.directory?(s) }
+    files = files.collect { |c| File.join(p,c) }
+  else
+    files = [p]
+  end
+
+  doc = WIN32OLE.new('MODI.Document')
+  files.each do |f|
+    # TODO - balance image color
+
+    doc.Create(f)
+    begin
+      doc.OCR()
+
+      for image in doc.Images
+        data[f] = image.Layout.Text
+      end
+    rescue Exception => e
+      #data[f] = e.message
+    end
+  end
+end
+print JSON.pretty_generate(data)
